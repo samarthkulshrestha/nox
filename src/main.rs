800597dd9bba8ff59c6ed3e5df5c61205ec246a2
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2023-12-18 18:27:35 +0530

    initial commit; prgm can ptrn match + subst exprs

---
+++ src/main.rs
@@ -0,0 +1,146 @@
+use std::fmt;
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, PartialEq)]
+enum Expr {
+    Sym(String),
+    Fun(String, Vec<Expr>),
+}
+
+impl fmt::Display for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Expr::Sym(name) => write!(f, "{}", name),
+            Expr::Fun(name, args) => {
+                write!(f, "{}(", name)?;
+                for (i, arg) in args.iter().enumerate() {
+                    if i > 0 { write!(f, ", ")? }
+                    write!(f, "{}", arg)?;
+                }
+                return write!(f, ")");
+            },
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Rule {
+    head: Expr,
+    body: Expr,
+}
+
+impl Rule {
+    fn apply_all(&self, expr: &Expr) -> Expr {
+        if let Some(bindings) = pattern_match(&self.head, expr) {
+            substitute_bindings(&bindings, &self.body)
+        } else {
+            use Expr::*;
+            match expr {
+                Sym(_) => expr.clone(),
+                Fun(name, args) => {
+                    let mut new_args = Vec::new();
+                    for arg in args {
+                        new_args.push(self.apply_all(arg));
+                    }
+                    return Fun(name.clone(), new_args);
+                }
+            }
+        }
+    }
+}
+
+impl fmt::Display for Rule {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        return write!(f, "{} = {}", self.head, self.body);
+    }
+}
+
+fn substitute_bindings(bindings: &Binding, expr: &Expr) -> Expr {
+    use Expr::*;
+    match expr {
+        Sym(name) => {
+            if let Some(value) = bindings.get(name) {
+                value.clone()
+            } else {
+                expr.clone()
+            }
+        },
+
+        Fun(name, args) => {
+            let new_name = match bindings.get(name) {
+                Some(Sym(new_name)) => new_name.clone(),
+                None => name.clone(),
+                Some(_) => panic!("expected symbol instead of functor name."),
+            };
+            let mut new_args = Vec::new();
+            for arg in args {
+                new_args.push(substitute_bindings(bindings, &arg));
+            }
+            return Fun(new_name, new_args);
+        }
+    }
+}
+
+type Binding = HashMap<String, Expr>;
+fn pattern_match(pattern: &Expr, value: &Expr) -> Option<Binding> {
+    fn pattern_match_impl(pattern: &Expr, value: &Expr, bindings: &mut Binding) -> bool {
+        use Expr::*;
+        match (pattern, value) {
+            (Sym(name), _) => {
+                if let Some(bound_value) = bindings.get(name) {
+                    return bound_value == value;
+                } else {
+                    bindings.insert(name.clone(), value.clone());
+                    return true;
+                }
+            },
+            (Fun(name1, args1), Fun(name2, args2)) => {
+                if name1 == name2 && args1.len() == args2.len() {
+                    for i in 0..args1.len() {
+                        if !pattern_match_impl(&args1[i], &args2[i], bindings) {
+                            return false;
+                        }
+                    }
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            _ => { return false; },
+        }
+    }
+
+    let mut bindings = HashMap::new();
+
+    if pattern_match_impl(pattern, value, &mut bindings) {
+        Some(bindings)
+    } else {
+        None
+    }
+}
+
+fn main() {
+    use Expr::*;
+    // swap(pair(a, b)) = pair(b, a)
+    let swap = Rule {
+                    head: Fun("swap".to_string(),
+                            vec![Fun("pair".to_string(),
+                            vec![Sym("a".to_string()), Sym("b".to_string())])]),
+                    body: Fun("pair".to_string(),
+                            vec![Sym("b".to_string()), Sym("a".to_string())]),
+    };
+
+    let expr = Fun("foo".to_string(),
+                vec![Fun("swap".to_string(),
+                vec![Fun("pair".to_string(),
+                vec![Fun("f".to_string(), vec![Sym("a".to_string())]),
+                Fun("g".to_string(), vec![Sym("b".to_string())])])]),
+                Fun("swap".to_string(),
+                vec![Fun("pair".to_string(),
+                vec![Fun("q".to_string(), vec![Sym("c".to_string())]),
+                Fun("z".to_string(), vec![Sym("d".to_string())])])])]);
+
+    println!("rule:  {}", swap);
+    println!("expr:  {}", expr);
+    println!("expr': {}", swap.apply_all(&expr));
+}
