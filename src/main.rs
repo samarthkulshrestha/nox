87640812cdba900c1c974330abf0ba16ea75c829
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2023-12-19 20:40:39 +0530

    factoring out the lexer module

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 use std::fmt;
+use std::iter::Peekable;
 use std::io::{stdin, stdout, Write};
 use std::collections::HashMap;
-use std::iter::Peekable;
 
-#[derive(Debug, Clone)]
-struct Loc {
-    file_path: Option<String>,
-    row: usize,
-    col: usize,
-}
+mod lexer;
+use lexer::*;
 
 #[derive(Debug, Clone, PartialEq)]
 enum Expr {
@@ -224,105 +220,6 @@ mod tests {
     }
 }
 
-#[derive(Debug, PartialEq, Clone, Copy)]
-enum TokenKind {
-    Sym,
-    OpenParen,
-    CloseParen,
-    Comma,
-    Equals,
-    Invalid,
-}
-
-impl fmt::Display for TokenKind {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use TokenKind::*;
-        match self {
-            Sym => write!(f, "symbol"),
-            OpenParen => write!(f, "'('"),
-            CloseParen => write!(f, "')'"),
-            Comma => write!(f, "','"),
-            Equals => write!(f, "'='"),
-            Invalid => write!(f, "invalid token"),
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-struct Token {
-    kind: TokenKind,
-    text: String,
-    loc: Loc,
-}
-
-struct Lexer<Chars: Iterator<Item=char>> {
-    chars: Peekable<Chars>,
-    invalid: bool,
-    file_path: Option<String>,
-    lnum: usize,
-    bol: usize,
-    cnum: usize,
-}
-
-impl<Chars: Iterator<Item=char>> Lexer<Chars> {
-    fn from_iter(chars: Chars) -> Self {
-        Self {
-            chars: chars.peekable(),
-            invalid: false,
-            file_path: None,
-            lnum: 0,
-            bol: 0,
-            cnum: 0,
-        }
-    }
-    fn loc(&self) -> Loc {
-        Loc {
-            file_path: self.file_path.clone(),
-            row: self.lnum,
-            col: self.cnum - self.bol,
-        }
-    }
-}
-
-impl<Chars: Iterator<Item=char>> Iterator for Lexer<Chars> {
-    type Item = Token;
-    fn next(&mut self) -> Option<Token> {
-        if self.invalid { return None }
-
-        while let Some(x) = self.chars.next_if(|x| x.is_whitespace()) {
-            self.cnum += 1;
-            if x == '\n' {
-                self.lnum += 1;
-                self.bol = self.cnum;
-            }
-        }
-
-        let loc = self.loc();
-        let x = self.chars.next()?;
-        self.cnum += 1;
-
-        let mut text = x.to_string();
-        match x {
-                '(' => Some(Token {kind: TokenKind::OpenParen, text, loc}),
-                ')' => Some(Token {kind: TokenKind::CloseParen, text, loc}),
-                ',' => Some(Token {kind: TokenKind::Comma, text, loc}),
-                '=' => Some(Token {kind: TokenKind::Equals, text, loc}),
-                _ => {
-                    if !x.is_alphanumeric() {
-                        self.invalid = true;
-                        return Some(Token{kind: TokenKind::Invalid, text, loc});
-                    } else {
-                        while let Some(x) = self.chars.next_if(|x| x.is_alphanumeric()) {
-                            self.cnum += 1;
-                            text.push(x)
-                        }
-                    return Some(Token{kind: TokenKind::Sym, text, loc});
-                }
-            }
-        }
-    }
-}
-
 fn main() {
     println!("");
 
@@ -355,6 +252,8 @@ fn main() {
                 println!("ERROR: expected {} but encountered EOF.", expected)
             }
         }
+
+        println!("");
     }
 
 }
